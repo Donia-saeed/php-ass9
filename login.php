0160e906eb1dda70876bc62630b5e25f1143b356
Donia-saeed <doniasaeed565@gmail.com>
2024-04-18 05:14:49 +0200

    first edit

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $accounts = file_get_contents('data.json'); // get data from file convert it to array
+    $accounts = json_decode($accounts, true);
+    $flag = 0;
+    foreach ($accounts as $account) {
+        if ($account['username'] == $username && password_verify($password, $account['password'])) {
+            $flag = 1;
+            break;
+        }
+    }
+    if ($flag) {
+        include 'index.php';
+    } else {
+        echo 'wrong username or password';
+    }
+}
+?>
---
+++ register.php
@@ -0,0 +1,21 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $password = $_POST['password'];
+    $comPassword = $_POST['comPassword'];
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    $comHashedPassword = password_hash($comPassword, PASSWORD_DEFAULT);
+
+    $_POST['password'] = $hashedPassword;//replace
+    $_POST['comPassword'] = $comHashedPassword;//replace
+
+    $data = file_get_contents('data.json'); // get data from file convert it to array
+    $data = json_decode($data, true);
+    $data[] = $_POST; // push new $_POST into old $data[] array
+    $data = json_encode($data, JSON_PRETTY_PRINT); //store data in encoding
+    file_put_contents('data.json', $data);
+}
+
+header('Location: login.html');
+exit(); // Always exit after a header redirect
+?>
